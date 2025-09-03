c66314600fa3a4c9d27d92462947d326574f9041
MelikaSadatMirdamadi <mirdamadimelika3@gmail.com>
2025-09-03 16:44:24 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+from langgraph.prebuilt  import create_react_agant
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def main():
+    print("Hello")
+
+
